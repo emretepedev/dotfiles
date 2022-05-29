3e539554cda8670d284e6a0426f0b1335482ddce
Emre Tepe <emretepedev@gmail.com>
2022-05-29 18:51:24 +0300

    fixed polybar powermenu and refactored

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,10 +19,6 @@ alias gittirgit2="~/pushToGitHub/commitToGitHub.sh && ~/pushToGitHub/pushToGitHu
 alias gp2="~/pushToGitHub.sh && git push"
 alias gcmsg2="~/pushToGitHub/commitToGitHub.sh && git commit -m"
 
-# Network Aliasses
-alias vpn="sudo openvpn ~/$XDG_CONFIG_HOME/openvpn/vpn.ovpn"
-alias ipp="dig +short myip.opendns.com @resolver1.opendns.com"
-
 # Tmuxinator Aliasses
 alias mux="tmuxinator"
 alias muxs="mux start"
@@ -31,37 +27,14 @@ alias muxd="mux delete"
 alias muxn="mux new"
 alias muxe="mux edit"
 
-# Bundler aliasses
-alias be="bundle exec"
-alias ber="be rails"
-alias berg="ber g"
-
 # Copy Aliasses
 alias cprpf="cp -rpf"
 
 # Fuck alias
 eval $(thefuck --alias)
-alias whoamfckni="neofetch | lolcat -ad 2"
-
-# TTRV
-alias ttrvnsfw="cp $XDG_CONFIG_HOME/ttrv/spyworld $XDG_DATA_HOME/ttrv/refresh-token && ttrv"
-alias reddit="cp $XDG_CONFIG_HOME/ttrv/ugorur $XDG_DATA_HOME/ttrv/refresh-token && ttrv"
 
 # Nvim
 alias vim="nvim"
-alias svim="sudoedit"
-
-# Dropdowns
-alias fehd="feh --title=mpvfloat"
-alias mpvd="mpv --title=mpvfloat"
-
-# New SSL
-alias newssl="sudo certbot --authenticator standalone --installer nginx --pre-hook \"systemctl stop nginx.service\" --post-hook \"systemctl start nginx.service\""
-
-# For Remote Server
-alias s1="sudo openvpn .config/openvpn/bixos.ovpn"
-alias s2="ssh work"
-alias s3="sudo killall openvpn"
 
 # Lazygit
 alias lg="lazygit"
